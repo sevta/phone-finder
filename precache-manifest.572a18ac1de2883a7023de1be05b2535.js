e69ef4164af4e1a9fea1e792b67ea8ca448d9d97
sevta <sevtasaputra@gmail.com>
2019-02-06 23:17:10 +0700

    deploy

---
+++ precache-manifest.572a18ac1de2883a7023de1be05b2535.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/phone-finder/robots.txt"
+  },
+  {
+    "revision": "d78cffc36e4e7dd264b8",
+    "url": "/phone-finder/js/chunk-vendors.74b5d0a1.js"
+  },
+  {
+    "revision": "34011e6b6768473ed563",
+    "url": "/phone-finder/js/app.7cb9e7c5.js"
+  },
+  {
+    "revision": "08cd12cf056413df2b67759bf4e88393",
+    "url": "/phone-finder/index.html"
+  },
+  {
+    "revision": "34011e6b6768473ed563",
+    "url": "/phone-finder/css/app.cd2c5eb7.css"
+  }
+];
